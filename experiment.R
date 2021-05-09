474ca686d8f2c3e72534a427904e4ed8c5c168d7
jxu <7989982+jxu@users.noreply.github.com>
2021-05-09 02:13:32 -0400

    Plot reference 1D dist

--- experiment.R
+++ experiment.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(gridExtra)
 
 set.seed(10716)
 
@@ -11,16 +12,17 @@ uniform_matrix <- function(nrow, ncol, min, max) {
 }
 
 # simple random weights one hidden layer NN, described in Neal (1996)
+# low weight for bias like in Jacot et al (2018)
 one_layer_nn <- function(x, hidden_dim, output_dim) {
   input_dim <- length(x)
   
   # u, a: hidden layer weights, bias
   # v, b: output layer weights, bias
   
-  u <- uniform_matrix(hidden_dim, input_dim, -1, 1)  
+  u <- gaussian_matrix(hidden_dim, input_dim, 0, 1)  
   a <- rnorm(hidden_dim, 0, 1)  
-  v <- gaussian_matrix(output_dim, hidden_dim, 0, hidden_dim^(-1/2)) 
-  b <- rnorm(output_dim, 0, 1) 
+  v <- gaussian_matrix(output_dim, hidden_dim, 0, 1) / sqrt(hidden_dim)
+  b <- 0.1 * rnorm(output_dim, 0, 1) 
   
   h <- tanh(a + u %*% x)
   y <- b + v %*% h
@@ -29,18 +31,25 @@ one_layer_nn <- function(x, hidden_dim, output_dim) {
   
 }
 
+
 # simulate many networks to see output distribution
 # uses fixed data model
 
 # 1D output
-x <- rep(1,10)
-sim_1d_5 <- data.frame(x=replicate(10000, one_layer_nn(x, 5, 1)))
-ggplot(sim_1d_5, aes(x = x)) +
-  geom_density()
+simulate_1d <- function(hidden_dim, trials=1000) {
+  x <- 1  # fixed input
+  samp <- replicate(trials, one_layer_nn(x, hidden_dim, 1))
+  ggplot(data.frame(x = samp), aes(x = x)) +
+    geom_density() +
+    coord_cartesian(xlim=c(-3,3)) +
+    stat_function(fun=dnorm, xlim=c(-3,3), args = list(mean=0, sd=0.7), color="blue") +
+    ggtitle(paste0("hidden=", hidden_dim))
+}
 
-sim_1d_1000 <- data.frame(x=replicate(10000, one_layer_nn(x, 1000, 1)))
-ggplot(sim_1d_1000, aes(x = x)) +
-  geom_density()
+
+grobs <- lapply(c(1, 2, 5, 100), simulate_1d)
+grid.arrange(grobs=grobs)
+
 
 
 # 2D output
