1d80767eae51afa910b5526c6f915dec66efcde3
jxu <7989982+jxu@users.noreply.github.com>
2021-05-09 20:00:21 -0400

    Use ReLU activation and cleanup 2d plotting

--- experiment.R
+++ experiment.R
@@ -12,19 +12,19 @@ uniform_matrix <- function(nrow, ncol, min, max) {
 }
 
 # simple random weights one hidden layer NN, described in Neal (1996)
-# low weight for bias like in Jacot et al (2018)
+# use ReLU instead of tanh to make convergence clearer, though convergence holds for both
+# low weight for Guassian bias to give more weight to weights, like in Jacot et al (2018)
 one_layer_nn <- function(x, hidden_dim, output_dim) {
   input_dim <- length(x)
   
   # u, a: hidden layer weights, bias
   # v, b: output layer weights, bias
-  
   u <- gaussian_matrix(hidden_dim, input_dim, 0, 1)  
   a <- rnorm(hidden_dim, 0, 1)  
   v <- gaussian_matrix(output_dim, hidden_dim, 0, 1) / sqrt(hidden_dim)
   b <- 0.1 * rnorm(output_dim, 0, 1) 
   
-  h <- tanh(a + u %*% x)
+  h <- pmax(a + u %*% x, 0)
   y <- b + v %*% h
   
   return(as.numeric(y))
@@ -36,34 +36,37 @@ one_layer_nn <- function(x, hidden_dim, output_dim) {
 # uses fixed data model
 
 # 1D output
-simulate_1d <- function(hidden_dim, trials=1000) {
-  x <- 1  # fixed input
+plot_sim_1d <- function(hidden_dim, trials=1000) {
+  x <- 1  # fixed data
   samp <- replicate(trials, one_layer_nn(x, hidden_dim, 1))
   ggplot(data.frame(x = samp), aes(x = x)) +
     geom_density() +
     coord_cartesian(xlim=c(-3,3)) +
-    stat_function(fun=dnorm, xlim=c(-3,3), args = list(mean=0, sd=0.7), color="blue") +
+    stat_function(fun=dnorm, xlim=c(-3,3), args = list(mean=0, sd=1.0), color="blue") +
     ggtitle(paste0("hidden=", hidden_dim))
 }
 
 
-grobs <- lapply(c(1, 2, 5, 100), simulate_1d)
+grobs <- lapply(c(1, 5, 10, 1000), plot_sim_1d)
 grid.arrange(grobs=grobs)
 
 
-
 # 2D output
-sim_2d_5 <- replicate(10000, one_layer_nn(x, 5, 2))
-sim_2d_5 <- data.frame(x=sim_2d_5[1,], y=sim_2d_5[2,])
-ggplot(sim_2d_5, aes(x=x, y=y)) +
-  geom_density_2d() + 
-  coord_cartesian(xlim=c(-3,3), ylim=c(-3,3))
+plot_sim_2d <- function(hidden_dim, trials=1000) {
+  x1 <- c(0,1)  # fixed data
+  x2 <- c(1,0)
+  samp1 <- replicate(trials, one_layer_nn(x1, hidden_dim, 1))
+  samp2 <- replicate(trials, one_layer_nn(x2, hidden_dim, 1))
+  
+  df <- data.frame(x=samp1, y=samp2)
+  ggplot(df, aes(x=x, y=y)) +
+    stat_density_2d(aes(fill = ..level..), geom = "polygon") +
+    coord_cartesian(xlim=c(-3,3), ylim=c(-3,3)) +
+    ggtitle(paste0("hidden=", hidden_dim))
+  
+}
 
-
-sim_2d_100 <- replicate(10000, one_layer_nn(x, 100, 2))
-sim_2d_100 <- data.frame(x=sim_2d_100[1,], y=sim_2d_100[2,])
-ggplot(sim_2d_100, aes(x=x, y=y)) +
-  geom_density_2d() + 
-  coord_cartesian(xlim=c(-3,3), ylim=c(-3,3))
+grobs <- lapply(c(1, 5, 10, 1000), plot_sim_2d)
+grid.arrange(grobs=grobs)
 
 
